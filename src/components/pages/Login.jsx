2b87a792c55d0869041600272490015630c8c6a0
Karely3412 <karely@devpipeline.com>
2023-12-30 21:49:37 -0700

    added onChange & state to login page

--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -1,6 +1,11 @@
+import { useState } from "react";
+
 import LoginImage from "../../assets/images/login-image.jpg";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <div className="login-container">
       <div className="image-wrapper">
@@ -8,8 +13,16 @@ const Login = () => {
       </div>
       <div className="login-wrapper">
         <h1>Logo</h1>
-        <input placeholder="Email"></input>
-        <input placeholder="Password"></input>
+        <input
+          placeholder="Email"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        ></input>
+        <input
+          placeholder="Password"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        ></input>
         <button>Login</button>
       </div>
     </div>
